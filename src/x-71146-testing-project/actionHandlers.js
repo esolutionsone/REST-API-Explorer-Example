06f85c5b977d9f26e58ec4b499cbed046a4fe0f8
Jacob Laux <jake@esolutionsone.com>
2023-05-25 09:34:15 -0500

    Fixed Action actionHandlers

--- src/x-71146-testing-project/actionHandlers.js
+++ src/x-71146-testing-project/actionHandlers.js
@@ -4,25 +4,24 @@ import { createHttpEffect } from "@servicenow/ui-effect-http";
 const { COMPONENT_BOOTSTRAPPED } = actionTypes;
 
 export default {
-    [COMPONENT_BOOTSTRAPPED]: ({ dispatch }) => {
-        dispatch("FETCH_TABLES", {
+    [COMPONENT_BOOTSTRAPPED]: ({ dispatch }) => {        
+        console.log('testing in bootstraped')
+        dispatch('FETCH_TABLES', {
             tableName: "sys_db_object",
-            sysparm_display_value: "all",
         });
     },
-    FETCH_TABLES: createHttpEffect("api/now/table/:tableName", {
+    'FETCH_TABLES': createHttpEffect("api/now/table/:tableName", {
         method: "GET",
         pathParams:  [ "tableName" ],
-        queryParams: [ "sysparm_display_value" ],
         successActionType: "SET_TABLES_VALUE",
         errorActionType: "LOG_ERROR",
     }),
-    SET_TABLES_VALUE: ({ action, updateState }) => {
+    'SET_TABLES_VALUE': ({ action, updateState }) => {
         console.log("hey");
         console.log(action.payload.result);
         updateState({
           tables: "testing"
         });
     },
-    LOG_ERROR: ({ action }) => console.error("LOG_ERROR", action.payload.msg, action.payload.data),
+    'LOG_ERROR': ({ action }) => console.error("LOG_ERROR", action.payload.msg, action.payload.data),
 }
