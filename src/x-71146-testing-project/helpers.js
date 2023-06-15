71bda2e28ae4036bf3ffadf7487526a86b3f5651
Jacob Laux <jake@esolutionsone.com>
2023-06-15 10:58:12 -0500

    Updating helpers and actionHandlers commments

--- src/x-71146-testing-project/helpers.js
+++ src/x-71146-testing-project/helpers.js
@@ -97,6 +97,7 @@ export const fetchValues = debounce(( updateState, event, table, limit, dispatch
         tables: []
     });
 });
+/* Called by REST API "Main" Component when Send button is pressed */ 
 export const sendRest = debounce(( updateState, state, dispatch) => {
     /* This funciton uses debounce to ensure the processFetch function isn't continually
     called. It should only call once every 300ms. */
@@ -130,17 +131,28 @@ export const sendRest = debounce(( updateState, state, dispatch) => {
         default:
             break;
     }
-    /* Set loading to true and required to false and call processREST function and update state */
+    /* Set loading to true and required to false update state. Call processREST function and  */
     updateState({
         loading:  true,
         required: false
     });
     processREST(updateState, state, dispatch);
 });
+/* Called by PostFields Component */
 export const updateRowFields = ( updateState, state, action, index = 0 ) => {
+    /* Destructuring request_fields and request_fields_index from state & initializing vars 
+            The index_num will continually increment to avoid potentially creating duplicate 
+            keys >> the index only ever increasing on add (and not decrimenting on remove) will
+            ensure the unique input field names.
+        
+            The request_fields hold the index values and the actual field name and values to be used
+            in the post request. 
+    */
     const { request_fields, request_fields_index } = state;
     let index_num          = request_fields_index;
     let new_request_fields = [...request_fields];
+    /* If adding, we increment the index and we build a new empty row with the new index > this will
+    trigger a re-render to add a new set of empty field and value input fields on the PostFields Component*/
     if (action === "add"){
         index_num += 1;
         new_request_fields.push({
@@ -150,29 +162,46 @@ export const updateRowFields = ( updateState, state, action, index = 0 ) => {
             "value":       ""
         });
     } else if (action === "remove"){
+        /* else if we're removing, simply slice the clicked trashcan index out of the array */
         let temp_list      = new_request_fields.slice(0,index).concat(new_request_fields.slice(index+1));
         new_request_fields = [...temp_list];
     }
+    /* Update state with teh new request fields and new/current index number (depending on add or remove) */ 
     updateState({
         request_fields:       new_request_fields,
         request_fields_index: index_num       
     })
 }
+/* Called by fetchValues function above */
 let processFetch = ( event, table, limit, dispatch ) => {
+    /* Dispatch FETCH_VALUES action to the actionHandlers. tableName, sysparm_limit, and 
+    sysparm_query variables are set for the dispatch.*/
     dispatch("FETCH_VALUES", {
         tableName:     table,
         sysparm_limit: limit,
         sysparm_query: 'labelSTARTSWITH'+event
     });
 }
+/* Called by the sendRest function above */
 let processREST =  ( updateState, state, dispatch ) => {
+    /* Destructuring method, selected_table, query, and rest_fields from state*/
     const { method, selected_table, query, request_fields } = state;
+    /* Send either a GET or POST dispatch depending on the selected method */
     if (method === "GET"){
+        /*  if GET 
+            Dispatch the REST_GET action to the actionHandlers with the tableName 
+            and sysparm_query variables */
         dispatch("REST_GET", {
             tableName:     selected_table,
             sysparm_query: query
         })   
     } else if (method === "POST"){
+        /*  else if POST
+            Create the post body by looping through request_fields (which is populated
+            by the PostFields component) > This takes the field and value from each 
+            request_fields object and creates the POST body for the REST message. 
+            The request body is updated in the state and then the REST_POST action is 
+            dispatched to the actionHandlers */
         let post_request_body = {};
         request_fields.forEach( field => {
             post_request_body[field['field']] = field['value'];
@@ -186,20 +215,34 @@ let processREST =  ( updateState, state, dispatch ) => {
         })
     }
 }
+/* Called by the TypeAheadReference Component */
 export const selectValue = ( updateState, name, label, item ) => {
+    /* To make this comonent more generic, we added the ability to pass in
+    an item > this allows us to call a different function based on the specific 
+    typeahead that's selecteed > we only have one in our application. 
+    There are definitely better / more dynamic ways to do this but we felt this 
+    was more easily readable and a simple solution for our example component.*/
     if ( item === 'table' ){
         selectTable( updateState, name, label );
     }
     //else if ( item === 'ex.' ){}
 }
+/* Called by the selectValue function above*/
 export const selectTable = ( updateState, name, label ) => {
+    /* Update state with the selected table details and clear the 
+    dropdown value in the tables array */
     updateState({
         table:          label,
         selected_table: name,
         tables:         []
     });
 }
+/* Used by the fetchValues and sendRest functions above */
 function debounce(func, timeout = 300){
+    /* This function will call the function that is passed in after the timer 
+    times out >> this is indended to reduce calls to the server and not allow 
+    the user to "spam click" the send button or cause the on keyup function to 
+    send a call with every keypress */
     let timer;
     return (...args) => {
         clearTimeout(timer);
