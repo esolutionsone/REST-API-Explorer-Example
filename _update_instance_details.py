c58c611044b8f03504e0e54a3ae45363ab50c03e
Jacob Laux <jake@esolutionsone.com>
2023-06-21 12:57:30 -0500

    fixing company app name

--- _update_instance_details.py
+++ _update_instance_details.py
@@ -25,10 +25,10 @@ def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
 def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
 
 # Specify the root directory and the old_scope_name you want to match
-scope_name     = '/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py' # old_scope_name from our original working file
+scope_name     = '853443' # old_scope_name from our original working file
 prGreen("\nCurrent appcreator copmany code: x-" + scope_name + "-")
 prLightPurple("\nThis can be found by navigating to \"sys_properties.list\" in the filter navigator of your ServiceNow instance and searching for the property named \"glide.appcreator.company.code\".")
-prLightPurple("\nFor a developer instance, this will likely be a string of numbers! If you're using an organizational instance, it will most likely be a shorthand for your company (for example, ours is esg). \n\nIf you can't find your copmany code, you can try to deploy the component and an error should show the company code.\nFor example, here's an example of the error when deploying to the wrong Personal Developer Instance\n\"ERROR in Component tag name \"x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project\" \nmust start with the vendor prefix \"x-71146-\" \nIn this case, 71146 would be the code you enter for scope name!")
+prLightPurple("\nFor a developer instance, this will likely be a string of numbers! If you're using an organizational instance, it will most likely be a shorthand for your company (for example, ours is esg). \n\nIf you can't find your copmany code, you can try to deploy the component and an error should show the company code.\nFor example, here's an example of the error when deploying to the wrong Personal Developer Instance\n\"ERROR in Component tag name \"x-853443-testing-project\" \nmust start with the vendor prefix \"x-71146-\" \nIn this case, 71146 would be the code you enter for scope name!")
 
 new_scope_name = input("Enter your appcreator company code: ")  # User Input to get new instance scope Use raw_input() in Python 2
 print("\n")
@@ -55,21 +55,21 @@ else:
 #Files & Directories to scrub/replace (windows & mac paths below)
 mac_files           = [ './now-ui.json',
                         './src/index.js',
-                        './src/x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project/index.js',
-                        './src/x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project/__tests__/test.x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project.js',
+                        './src/x-853443-testing-project/index.js',
+                        './src/x-853443-testing-project/__tests__/test.x-853443-testing-project.js',
                         './README.md',
                         './_update_instance_details.py' ]
-mac_dirs_and_files  = [ './src/x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project/__tests__/test.x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project.js',
-                        './src/x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project' ]
+mac_dirs_and_files  = [ './src/x-853443-testing-project/__tests__/test.x-853443-testing-project.js',
+                        './src/x-853443-testing-project' ]
 
 win_files           = [ '.\\now-ui.json',
                         '.\\src\\index.js',
-                        '.\\src\\x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project\\index.js',
-                        '.\\src\\x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project\\__tests__\\test.x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project.js',
+                        '.\\src\\x-853443-testing-project\\index.js',
+                        '.\\src\\x-853443-testing-project\\__tests__\\test.x-853443-testing-project.js',
                         '.\\README.md',
                         '.\\_update_instance_details.py' ]
-win_dirs_and_files = [  '.\\src\\x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project\\__tests__\\test.x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project.js',
-                        '.\\src\\x-/usr/bin/python3 /Users/jake/Documents/Development/REST-API-EXPLORER/REST-API-Explorer-Example/_update_instance_details.py-testing-project' ]
+win_dirs_and_files = [  '.\\src\\x-853443-testing-project\\__tests__\\test.x-853443-testing-project.js',
+                        '.\\src\\x-853443-testing-project' ]
 
 # If we're running Linux/Mac based, use mac files, elif we're using Windows, use Windows filepaths
 if  os.name == 'posix':
