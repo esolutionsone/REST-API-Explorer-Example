734fb811de9b657be87defc782e1c9753134a1b5
Jacob Laux <jake@esolutionsone.com>
2023-06-21 13:40:26 -0500

    Updating path

--- _update_instance_details.py
+++ _update_instance_details.py
@@ -28,7 +28,7 @@ def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
 scope_name     = '853443' # old_scope_name from our original working file
 prGreen("\nCurrent appcreator copmany code: x-" + scope_name + "-")
 prLightPurple("\nThis can be found by navigating to \"sys_properties.list\" in the filter navigator of your ServiceNow instance and searching for the property named \"glide.appcreator.company.code\".")
-prLightPurple("\nFor a developer instance, this will likely be a string of numbers! If you're using an organizational instance, it will most likely be a shorthand for your company (for example, ours is esg). \n\nIf you can't find your copmany code, you can try to deploy the component and an error should show the company code.\nFor example, here's an example of the error when deploying to the wrong Personal Developer Instance\n\"ERROR in Component tag name \"x-853443-testing-project\" \nmust start with the vendor prefix \"x-71146-\" \nIn this case, 71146 would be the code you enter for scope name!")
+prLightPurple("\nFor a developer instance, this will likely be a string of numbers! If you're using an organizational instance, it will most likely be a shorthand for your company (for example, ours is esg). \n\nIf you can't find your copmany code, you can try to deploy the component and an error should show the company code.\nFor example, here's an example of the error when deploying to the wrong Personal Developer Instance\n\"ERROR in Component tag name \"x-<scopename>-testing-project\" \nmust start with the vendor prefix \"x-71146-\" \nIn this case, 71146 would be the code you enter for scope name!")
 
 new_scope_name = input("Enter your appcreator company code: ")  # User Input to get new instance scope Use raw_input() in Python 2
 print("\n")
@@ -58,7 +58,7 @@ mac_files           = [ './now-ui.json',
                         './src/x-853443-testing-project/index.js',
                         './src/x-853443-testing-project/__tests__/test.x-853443-testing-project.js',
                         './README.md',
-                        './example/element.js'
+                        './example/element.js',
                         './_update_instance_details.py' ]
 mac_dirs_and_files  = [ './src/x-853443-testing-project/__tests__/test.x-853443-testing-project.js',
                         './src/x-853443-testing-project' ]
@@ -68,7 +68,7 @@ win_files           = [ '.\\now-ui.json',
                         '.\\src\\x-853443-testing-project\\index.js',
                         '.\\src\\x-853443-testing-project\\__tests__\\test.x-853443-testing-project.js',
                         '.\\README.md',
-                        '.\\example\\element.js'
+                        '.\\example\\element.js',
                         '.\\_update_instance_details.py' ]
 win_dirs_and_files = [  '.\\src\\x-853443-testing-project\\__tests__\\test.x-853443-testing-project.js',
                         '.\\src\\x-853443-testing-project' ]
